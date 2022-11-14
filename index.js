be1e15dd1aa566ec72e1ab1e7a94f4854e6d525b
wen-pin <iwuewqaj951@gmail.com>
2022-11-14 23:32:28 +0800

    test fix .env DATABASE_URL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // 連接資料庫
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
