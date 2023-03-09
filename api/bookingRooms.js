10a0a90ccca01cdb2375cf1c4927967a26649363
wen-pin <iwuewqaj951@gmail.com>
2023-03-09 08:52:11 +0800

    取得預定房間資訊，新增token驗證

--- api/bookingRooms.js
+++ api/bookingRooms.js
@@ -4,7 +4,7 @@ const bookingRoomsController = require("../controllers/bookingRoomsController");
 const verifyJWT = require("../middleware/verifyJWT");
 
 router.get("/", bookingRoomsController.getAllBookingRooms);
-router.post("/v1", bookingRoomsController.getBookingRoom);
+router.post("/v1", verifyJWT, bookingRoomsController.getBookingRoom);
 router.post("/", bookingRoomsController.createNewBookingRoom);
 // router.patch("/:id", bookingRoomsController.updateRoom);
 // router.delete("/:id", bookingRoomsController.deleteRoom);
